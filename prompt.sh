b145c3a8d28f0c2e4ea0f1a742f1248190e961a9
Dayln Limesand <dayln.limesand@gmail.com>
2016-07-05 17:30:25 -0500

    Added prompt script

---
+++ prompt.sh
@@ -0,0 +1,68 @@
+# Begin /usr/share/defaults/etc/profile.d/50-prompt.sh
+
+endchar="\$"
+if [ "$UID" = "0" ]; then
+    endchar="#"
+fi
+
+FG="\[\033[38;5;111m\]"
+BG="\[\033[38;5;168m\]"
+AT="\[\033[38;5;235m\]"
+HCOLOR="\[\033[38;5;61m\]"
+
+# get current branch in git repo
+function parse_git_branch() {
+	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+	if [ ! "${BRANCH}" == "" ]
+	then
+		STAT=`parse_git_dirty`
+		echo "[${BRANCH}${STAT}]"
+	else
+		echo ""
+	fi
+}
+
+# get current status of git repo
+function parse_git_dirty {
+	status=`git status 2>&1 | tee`
+	dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+	untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+	ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
+	newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+	renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+	deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+	bits=''
+	if [ "${renamed}" == "0" ]; then
+		bits=">${bits}"
+	fi
+	if [ "${ahead}" == "0" ]; then
+		bits="*${bits}"
+	fi
+	if [ "${newfile}" == "0" ]; then
+		bits="+${bits}"
+	fi
+	if [ "${untracked}" == "0" ]; then
+		bits="?${bits}"
+	fi
+	if [ "${deleted}" == "0" ]; then
+		bits="x${bits}"
+	fi
+	if [ "${dirty}" == "0" ]; then
+		bits="!${bits}"
+	fi
+	if [ ! "${bits}" == "" ]; then
+		echo " ${bits}"
+	else
+		echo ""
+	fi
+}
+
+PS1="${HCOLOR}┌${BG}[\w] \`parse_git_branch\`\n${HCOLOR}└> ${FG}\u ${FG}$endchar \[\e[0m\]"
+
+unset FG
+unset BG
+unset AT
+unset HCOLOR
+shopt -s checkwinsize
+
+# End /usr/share/defaults/etc/profile.d/50-prompt.sh
