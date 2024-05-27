32d15066e30274d005de3eb807082f55513ae8c8
dayln <dayln@limesand.dev>
2024-05-27 13:42:37 -0500

    add wezterm configuration

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Tokyo Night Moon"
+
+config.enable_tab_bar = false
+
+-- and finally, return the configuration to wezterm
+return config
