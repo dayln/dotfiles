82b8127a8c7b860a67176314a9cbcaa0341285bf
Dayln <dayln@limesand.ev>
2025-01-04 19:47:54 -0600

    update which-key

--- neovim/.config/nvim/lua/plugins/which-key.lua
+++ neovim/.config/nvim/lua/plugins/which-key.lua
@@ -5,18 +5,14 @@ return {
     config = function()
       require('which-key').setup()
 
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = 'Code', mode = { 'n', 'x' } },
+        { '<leader>d', group = 'Document' },
+        { '<leader>s', group = 'Search' },
+        { '<leader>w', group = 'Workspace' },
+        { '<leader>t', group = 'Toggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'x' } },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 }
